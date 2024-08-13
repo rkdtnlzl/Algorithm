bae86c511244db1a8dc595a22cbc9e7751807e0b
강석호 <82876698+rkdtnlzl@users.noreply.github.com>
2024-08-13 23:04:29 +0900

    [level 2] Title: 피로도, Time: 25.28 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+func solution(_ k:Int, _ dungeons:[[Int]]) -> Int {
+    var result = 0
+    
+    var visited = [Bool](repeating: false, count: dungeons.count)
+    
+    func dfs(_ count: Int, _ pirodo: Int){
+        if result < count{
+            result = count
+        }
+        for i in 0..<dungeons.count{
+            if !visited[i] && dungeons[i][0] <= pirodo{
+                visited[i] = true
+                dfs(count + 1, pirodo - dungeons[i][1])
+                visited[i] = false
+            }
+        }
+    }
+    
+    dfs(0, k)
+    
+    return result
+}
