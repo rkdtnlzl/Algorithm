2acf6daff89e7b3b0ed276893b3f0e60263f7184
강석호 <82876698+rkdtnlzl@users.noreply.github.com>
2024-10-17 12:19:02 +0900

    [Silver IV] Title: 카드2, Time: 28 ms, Memory: 81588 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.swift
+++ 백준/Silver/2164. 카드2/카드2.swift
@@ -1,44 +1,15 @@
-struct QueuePointer<T> {
-    private var elements: [T] = []
-    private var front = 0
-    
-    var isEmpty: Bool {
-        elements.count < front + 1
-    }
-    
-    var count: Int {
-        elements.count - front
-    }
-    
-    func peek() -> T? {
-        front < elements.count ? elements[front] : nil
-    }
-    
-    mutating func enqueue(with element: T) {
-        elements.append(element)
-    }
-    
-    @discardableResult
-    mutating func dequeue() -> T? {
-        if !isEmpty {
-            defer { front += 1 }
-            return elements[front]
-        } else {
-            return nil
-        }
-    }
-}
-
-var queue = QueuePointer<Int>()
 let n = Int(readLine()!)!
+var queue: [Int] = []
+var front = 0
 
 for i in 1...n {
-    queue.enqueue(with: i)
+    queue.append(i)
 }
 
-while queue.count > 1 {
-    queue.dequeue()
-    queue.enqueue(with: queue.dequeue()!)
+while queue.count - front > 1 {
+    front += 1
+    queue.append(queue[front])
+    front += 1
 }
 
-print(queue.peek()!)
+print(queue[front])
